7a6f7b0ed3e550238ab5e725d5e67985afd8a261
masa <masahito.takeuchi@numan.com>
2026-08-31 22:03:00 +0200

    add position save, stamp, delete

--- pixel4/scripts/sandbox.js
+++ pixel4/scripts/sandbox.js
@@ -1,13 +1,19 @@
 import { getData } from '../../config.js'
 
+const SANDBOX_SAVE_DATA = 'sandbox-save-data'
+
 window.addEventListener('DOMContentLoaded', () => {
   const menu = document.getElementById('menu')
+  const stamp = document.querySelector('.stamp')
 
   const settings = {
     activeElement: null,
     elements: [],
     blocks: [],
-    artboard: null,
+    artboardWindow: null,
+    parameterWindow: null,
+    stampImg: null,
+    deleteMode: false,
   }
   class PageObject {
     constructor() {
@@ -29,7 +35,9 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   class Window extends PageObject {
-    constructor() {
+    constructor(
+      { size, pos } = { size: { w: 200, h: 100 }, pos: { x: 0, y: 0 } }
+    ) {
       super()
       this.el = Object.assign(document.createElement('div'), {
         className: 'window',
@@ -41,15 +49,28 @@ window.addEventListener('DOMContentLoaded', () => {
       this.id = 'window'
 
       document.body.appendChild(this.el)
-      settings.artboard = this.el.querySelector('.artboard')
+      settings.artboardWindow = this
+      this.artboard = this.el.querySelector('.artboard')
       settings.elements.push(this)
 
-      this.setSize({ w: 200, h: 100 })
+      this.setSize(size)
+      this.setPos(pos)
+
+      this.artboard.addEventListener('click', e => {
+        if (settings.stampImg) {
+          const { column, row } = settings.stampImg
+          const { x, y } = settings.artboardWindow.pos
+          new Block({
+            ...settings.stampImg,
+            pos: { x: e.pageX - column / 2 - x, y: e.pageY - row / 2 - y - 20 },
+          })
+        }
+      })
     }
   }
 
   class Parameter extends PageObject {
-    constructor() {
+    constructor(pos) {
       super()
       this.el = Object.assign(document.createElement('div'), {
         className: 'window',
@@ -62,8 +83,10 @@ window.addEventListener('DOMContentLoaded', () => {
       document.body.appendChild(this.el)
       this.content = this.el.querySelector('.artboard')
       settings.elements.push(this)
+      settings.parameterWindow = this
 
       this.setSize({ w: 200, h: 100 })
+      if (pos) this.setPos(pos)
     }
   }
 
@@ -73,24 +96,38 @@ window.addEventListener('DOMContentLoaded', () => {
       element => element.id === e.target.dataset.id
     )
     if (el) {
-      settings.activeElement = el
-      el.pointerPos = { x: e.pageX - el.pos.x, y: e.pageY - el.pos.y }
-      el.grabSize = {
-        w: el.size.w - el.pos.x,
-        h: el.size.h - el.pos.y,
+      if (settings.deleteMode) {
+        el.el.remove()
+        settings.elements = settings.elements.filter(element => element !== el)
+        settings.blocks = settings.blocks.filter(block => block !== el)
+      } else {
+        settings.activeElement = el
+        el.pointerPos = { x: e.pageX - el.pos.x, y: e.pageY - el.pos.y }
+        el.grabSize = {
+          w: el.size.w - el.pos.x,
+          h: el.size.h - el.pos.y,
+        }
       }
     }
   })
   ;['pointerup', 'pointercancel'].forEach(action => {
     window.addEventListener(action, () => {
       settings.activeElement = null
+      localStorage.setItem(
+        SANDBOX_SAVE_DATA,
+        JSON.stringify({
+          artboard: settings.artboardWindow,
+          parameter: settings.parameterWindow,
+          blocks: settings.blocks,
+        })
+      )
     })
   })
 
   window.addEventListener('pointermove', e => {
     e.target.setPointerCapture(e.pointerId)
     const el = settings.activeElement
-    if (el) {
+    if (el && !settings.stampImg) {
       if (e.target.classList.contains('size-handle')) {
         el.setSize({
           w: el.grabSize.w + (e.pageX - el.pointerPos.x),
@@ -102,14 +139,12 @@ window.addEventListener('DOMContentLoaded', () => {
           y: e.pageY - el.pointerPos.y,
         })
       }
+    } else {
+      stamp.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`
     }
   })
-
-  new Window()
-  new Parameter()
-
   class Block extends PageObject {
-    constructor({ dataUrl, column, row, name }) {
+    constructor({ dataUrl, column, row, name, id, pos, size }) {
       super()
       this.el = Object.assign(document.createElement('div'), {
         className: 'block',
@@ -117,13 +152,29 @@ window.addEventListener('DOMContentLoaded', () => {
       })
       settings.elements.push(this)
       settings.blocks.push(this)
+      this.dataUrl = dataUrl
       this.name = name
-      this.id = `${name}-${settings.blocks.filter(b => b.name === name).length}`
+      this.id =
+        id || `${name}-${settings.blocks.filter(b => b.name === name).length}`
       this.el.dataset.id = this.id
-      settings.artboard.appendChild(this.el)
+      settings.artboardWindow.artboard.appendChild(this.el)
 
-      this.setSize({ w: column, h: row })
+      this.setSize(size || { w: column, h: row })
+      if (pos) this.setPos(pos)
     }
+  }
+
+  const savedData = localStorage.getItem(SANDBOX_SAVE_DATA)
+
+  if (savedData) {
+    const data = JSON.parse(savedData)
+    new Window(data.artboard)
+    new Parameter(data.parameter.pos)
+
+    data.blocks.forEach(b => new Block(b))
+  } else {
+    new Window()
+    new Parameter()
   }
 
   getData().forEach(d => {
@@ -134,8 +185,21 @@ window.addEventListener('DOMContentLoaded', () => {
     menu.appendChild(box)
     box.dataset.size = `${d.column} x ${d.row}`
     box.addEventListener('click', () => {
+      stamp.innerHTML = `<img src="${d.dataUrl}" />`
+      settings.stampImg = d
       menu.hidePopover()
-      new Block(d)
     })
+  })
+
+  document.querySelector('.clear-stamp').addEventListener('click', () => {
+    stamp.innerHTML = ''
+    settings.stampImg = null
+  })
+
+  window.addEventListener('keydown', e => {
+    if (e.key.toLowerCase() === 'd') settings.deleteMode = true
+  })
+  window.addEventListener('keyup', e => {
+    if (e.key.toLowerCase() === 'd') settings.deleteMode = false
   })
 })
